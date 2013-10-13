65afc783b74bbda23a34809fdef2b0fc39cf8b35
liu316484231 <liu316484231@163.com>
2013-10-13 13:49:53 +0800

    fix bugs

--- primefaces/src/com/xiaodai/bean/CalculatorBean.java
+++ primefaces/src/com/xiaodai/bean/CalculatorBean.java
@@ -55,21 +55,29 @@ public class CalculatorBean implements Serializable{
 		double quarterinterstrate = monthinterestrate*3;
 		double quarter = this.timelimit/3;
 		double interest = 0;
-		if((Math.pow(1+monthinterestrate,this.timelimit)-1)==0||Math.pow(1+quarterinterstrate,quarter)-1==0){
-			return 0.0;
-		}
+//		if((Math.pow(1+monthinterestrate,this.timelimit)-1)==0||Math.pow(1+quarterinterstrate,quarter)-1==0){
+//			return 0.0;
+//		}
 		if(this.repaymentmode==1){
-			interest = this.money*monthinterestrate*Math.pow(1+monthinterestrate, this.timelimit)/(Math.pow(1+monthinterestrate,this.timelimit)-1)*this.timelimit-this.money;
-			double temp =interest*(1-this.managementrate/100)+this.bonusrate/100*this.money;
-			BigDecimal result = new BigDecimal(temp);
-		
-			return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
-		}else if(this.repaymentmode==2){
-			interest = this.money*quarterinterstrate*Math.pow(1+quarterinterstrate, quarter)/(Math.pow(1+quarterinterstrate,quarter)-1)*quarter-this.money;
-			double temp =interest*(1-this.managementrate/100)+this.bonusrate/100*this.money;
-			BigDecimal result = new BigDecimal(temp);
+			if(Math.pow(1+monthinterestrate,this.timelimit)-1==0){
+				return 0.0;
+			}else{
+				interest = this.money*monthinterestrate*Math.pow(1+monthinterestrate, this.timelimit)/(Math.pow(1+monthinterestrate,this.timelimit)-1)*this.timelimit-this.money;
+				double temp =interest*(1-this.managementrate/100)+this.bonusrate/100*this.money;
+				BigDecimal result = new BigDecimal(temp);
 			
-			return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+				return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+			}
+		}else if(this.repaymentmode==2){
+			if(Math.pow(1+quarterinterstrate,quarter)-1==0){
+				return 0.0;
+			}else{
+				interest = this.money*quarterinterstrate*Math.pow(1+quarterinterstrate, quarter)/(Math.pow(1+quarterinterstrate,quarter)-1)*quarter-this.money;
+				double temp =interest*(1-this.managementrate/100)+this.bonusrate/100*this.money;
+				BigDecimal result = new BigDecimal(temp);
+				
+				return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+			}
 		}else{
 			interest = this.money*monthinterestrate*this.timelimit;
 			double temp =interest*(1-this.managementrate/100)+this.bonusrate/100*this.money;
