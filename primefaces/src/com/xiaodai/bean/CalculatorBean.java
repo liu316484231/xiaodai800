da32617752c4d3017e39a1e8681a2ed0d1d76088
liu316484231 <liu316484231@163.com>
2013-10-18 14:15:30 +0800

    完成计算器功能

--- primefaces/src/com/xiaodai/bean/CalculatorBean.java
+++ primefaces/src/com/xiaodai/bean/CalculatorBean.java
@@ -14,6 +14,20 @@ public class CalculatorBean implements Serializable{
 	private int timelimit;
 	private double money;
 	private int repaymentmode=1;
+	private int option1;
+	private int option2;
+	public int getOption1() {
+		return option1;
+	}
+	public void setOption1(int option1) {
+		this.option1 = option1;
+	}
+	public int getOption2() {
+		return option2;
+	}
+	public void setOption2(int option2) {
+		this.option2 = option2;
+	}
 	public int getRepaymentmode() {
 		return repaymentmode;
 	}
@@ -50,43 +64,173 @@ public class CalculatorBean implements Serializable{
 	public void setMoney(double money) {
 		this.money = money;
 	}
-	public double calculator(){
-		double monthinterestrate = this.yearinterestrate/12/100;
-		double quarterinterstrate = monthinterestrate*3;
-		double quarter = this.timelimit/3;
-		double interest = 0;
-//		if((Math.pow(1+monthinterestrate,this.timelimit)-1)==0||Math.pow(1+quarterinterstrate,quarter)-1==0){
-//			return 0.0;
-//		}
-		if(this.repaymentmode==1){
-			if(Math.pow(1+monthinterestrate,this.timelimit)-1==0){
-				return 0.0;
-			}else{
-				interest = this.money*monthinterestrate*Math.pow(1+monthinterestrate, this.timelimit)/(Math.pow(1+monthinterestrate,this.timelimit)-1)*this.timelimit-this.money;
-				double temp =interest*(1-this.managementrate/100)+this.bonusrate/100*this.money;
-				BigDecimal result = new BigDecimal(temp);
+	public double calculateIncome(){
+		
+		double income;
+		double yinterest = 0;
+		double timelimit = this.timelimit;;
+		int paymode = this.repaymentmode;
+		if(this.option1==0||this.option2==0){
+			return 0.0;
+		}
+		if(this.option1 == 1){
+			yinterest = this.yearinterestrate;
+
+		}else if(this.option1 == 2){
+			yinterest = this.yearinterestrate*360;
+
+		}
+		
+		yinterest = yinterest*(1-this.managementrate/100);
+		if(this.option2==1){
 			
+			if(paymode==1){
+				if((Math.pow((1 + yinterest / 1200),timelimit)- 1)==0){
+					return 0.0;
+				}else{
+					if (yinterest > 0) {
+						income = (money * yinterest / 1200 * Math.pow((1 + yinterest / 1200), timelimit) /(Math.pow((1 + yinterest / 1200),timelimit)- 1) * timelimit - money + money * this.bonusrate / 100);
+					} else {
+						income = Math.round(money*this.bonusrate) / 100;
+					}
+				}
+				BigDecimal result = new BigDecimal(income);
 				return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
-			}
-		}else if(this.repaymentmode==2){
-			if(Math.pow(1+quarterinterstrate,quarter)-1==0){
-				return 0.0;
-			}else{
-				interest = this.money*quarterinterstrate*Math.pow(1+quarterinterstrate, quarter)/(Math.pow(1+quarterinterstrate,quarter)-1)*quarter-this.money;
-				double temp =interest*(1-this.managementrate/100)+this.bonusrate/100*this.money;
-				BigDecimal result = new BigDecimal(temp);
+			}else if(paymode==2){
+					double season = Math.ceil(timelimit / 3);
+					income = money * yinterest * (1 + season) / 800 + money * this.bonusrate / 100;
+					BigDecimal result = new BigDecimal(income);
+					return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
 				
+			}else if(paymode==3||paymode==4){
+					income = money * yinterest * timelimit/ 1200 + money * this.bonusrate / 100;
+					BigDecimal result = new BigDecimal(income);
+					return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+				
+			}
+			}else if(this.option2==2){
+				income = money * yinterest * timelimit / 36000 + money * this.bonusrate / 100;
+				BigDecimal result = new BigDecimal(income);
+				return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+		}
+
+		
+		return 0.0;
+		
+	}
+	public double calculateYearInterest(){
+		double yinterest = 0;
+		double timelimit = this.timelimit;;
+		int paymode = this.repaymentmode;
+		
+		if(this.option1==0||this.option2==0){
+			return 0.0;
+		}
+		
+		if(this.option1 == 1){
+			yinterest = this.yearinterestrate;
+		}else if(this.option1 == 2){
+			yinterest = this.yearinterestrate * 360;
+		}
+		
+		yinterest = yinterest*(1-this.managementrate/100);
+		if(this.option2==1){
+			//totalYearRate = 24.00 * prize / (limitTime + 1) + yearRate;
+			//flYearRate = (Math.pow((1 + totalYearRate / 1200), 12) - 1) * 100;
+			if(paymode==1){
+				double temp = 24*this.bonusrate/(timelimit+1)+yinterest;
+				BigDecimal result = new BigDecimal(temp);
+				return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+			}else if(paymode==2){
+				//totalYearRate = (yearRate * 3 + 24 * prize /(limitTime/3+1)) / 3
+				//totalYearRate = (yearRate * 3 + 24 * prize /(limitTime/3+1)) / 3;
+				double temp = (yinterest*3+24*this.bonusrate/(timelimit/3+1))/3;
+				BigDecimal result = new BigDecimal(temp);
+				return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+			
+				
+			}else if(paymode==3){
+				//totalYearRate = (yearRate * limitTime + 12 * prize) / limitTime;
+				double temp = (yinterest*timelimit+12*this.bonusrate)/timelimit;
+				BigDecimal result = new BigDecimal(temp);
+				return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+				
+			}else if(paymode==4){
+				//totalYearRate = yearRate + prize * 12 / limitTime;
+				double temp = yinterest+this.bonusrate*12/timelimit;
+				BigDecimal result = new BigDecimal(temp);
 				return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
 			}
-		}else{
-			interest = this.money*monthinterestrate*this.timelimit;
-			double temp =interest*(1-this.managementrate/100)+this.bonusrate/100*this.money;
+		
+		}else if(this.option2==2){
+			double temp = yinterest+this.bonusrate*360/timelimit;
 			BigDecimal result = new BigDecimal(temp);
+			return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+		}
+
+		
+		return 0.0;
+		
+	}
+	
+public double calculateCompoundInterest(){
+	double yinterest = 0;
+	double timelimit = this.timelimit;;
+	int paymode = this.repaymentmode;
+	
+	if(this.option1==0||this.option2==0){
+		return 0.0;
+	}
+	
+	if(this.option1 == 1){
+		yinterest = this.yearinterestrate;
+	}else if(this.option1 == 2){
+		yinterest = this.yearinterestrate * 360;
+	}
+	yinterest = yinterest*(1-this.managementrate/100);
+	if(this.option2==1){
+		//totalYearRate = 24.00 * prize / (limitTime + 1) + yearRate;
+		//flYearRate = (Math.pow((1 + totalYearRate / 1200), 12) - 1) * 100;
+		if(paymode==1){
+			//flYearRate = (Math.pow((1 + totalYearRate / 1200), 12) - 1) * 100;
+			double temp = (Math.pow(1+this.calculateYearInterest()/1200, 12)-1)*100;
+			BigDecimal result = new BigDecimal(temp);
+			return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+		}else if(paymode==2){
+			//totalYearRate = (yearRate * 3 + 24 * prize /(limitTime/3+1)) / 3;
+				//flYearRate = (Math.pow((1 + totalYearRate / 400), 4) - 1) * 100;
+			double temp = (Math.pow(1+this.calculateYearInterest()/400,4)-1)*100;
+			BigDecimal result = new BigDecimal(temp);
+			return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+		
 			
+		}else if(paymode==3||paymode==4){
+			//totalYearRate = (yearRate * limitTime + 12 * prize) / limitTime;
+			//flYearRate = (Math.pow((1 + totalYearRate / 1200 * limitTime), 12 / limitTime) - 1) * 100;
+			double temp = (Math.pow(1+this.calculateYearInterest()/1200*timelimit, 12/timelimit)-1)*100;
+			BigDecimal result = new BigDecimal(temp);
 			return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
 			
 		}
-		
-		
+	
+	}else if(this.option2==2){
+		//flYearRate = (Math.pow((1 + totalYearRate / 36500 * limitTime), 365 / limitTime) - 1) * 100;
+		double temp = (Math.pow(1+this.calculateYearInterest()/36500*timelimit, 365/timelimit)-1)*100;
+		BigDecimal result = new BigDecimal(temp);
+		return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
+
+	
+	return 0.0;
+	}
+	public double calculateMonthInterest(){
+		double temp =  this.calculateYearInterest()/12;
+		BigDecimal result = new BigDecimal(temp);
+		return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
+	public double calculateMonthCompoundInterest(){
+		double temp =  this.calculateCompoundInterest()/12;
+		BigDecimal result = new BigDecimal(temp);
+		return result.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
 	}
 }
